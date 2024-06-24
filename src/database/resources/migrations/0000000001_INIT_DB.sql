cf810ffd46e3ad31af731d3fd23e7bb53816f5ff
kudlayalexander <kudlay2014@yandex.ru>
2024-06-24 09:10:50 +0500

    fixed bugs with PG sql

--- src/database/resources/migrations/0000000001_INIT_DB.sql
+++ src/database/resources/migrations/0000000001_INIT_DB.sql
@@ -1,18 +1,17 @@
-CREATE TABLE "appointments" (
+CREATE TABLE IF NOT EXISTS "appointments" (
   "id" uuid PRIMARY KEY,
-  "meeting_date" datetime,
+  "meeting_date" timestamp,
   "place" varchar(400),
   "additional_info" varchar(400),
-  "teacher_login" varchar(40),
-  "student_login" varchar(40)
+  "teacher_login" varchar(40)
 );
 
-CREATE TABLE "requests" (
+CREATE TABLE IF NOT EXISTS "requests" (
   "id" uuid PRIMARY KEY,
-  "creation_time" datetime,
+  "creation_time" timestamp,
   "appointment_id" uuid,
   "student_login" varchar(40),
   "status" varchar(40)
 );
 
-ALTER TABLE "appointments" ADD FOREIGN KEY ("id") REFERENCES "requests" ("appointment_id") ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE "requests" ADD FOREIGN KEY ("appointment_id") REFERENCES "appointments" ("id") ON UPDATE CASCADE ON DELETE CASCADE;
